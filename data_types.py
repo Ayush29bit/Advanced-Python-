802e7fd455beac7faa61bffb7cf5e0a799513575
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-01-13 12:56:41 +0530

    data types

---
+++ data_types.py
@@ -0,0 +1,29 @@
+# Integers
+print(8)
+print(type(8))
+
+print(1e308) 
+
+# Floating-point numbers
+print(5.32)
+print(type(5.32))
+print(1.7e-5)
+
+# Booleans
+print(True)
+print(type(True))
+print(False)
+print(type(False))
+
+# Strings
+print(" hello world ")
+print(type(" hello world "))
+print('Python is fun!')
+print("""This is a multi-line
+string.""")
+
+# LISTS
+print([1,2,3,4,5])
+print(type([1,2,3,4,5]))
+print(["apple", "banana", "cherry"])
+print([1, "hello", 3.14, True])
