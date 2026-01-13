eec0f65bdf65ec33b24e14f42fc59a39127b5030
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-01-13 12:50:13 +0530

    basics covered

---
+++ basics.py
@@ -0,0 +1,13 @@
+str = " hello world "
+print(str)
+
+print(7)
+print(5.32)
+print(True)
+
+print("hello", 4, 543.3, False)
+
+print("hello", 4, 543.3, False, sep=" / ")
+
+print("hello", end="-")
+print("world")
