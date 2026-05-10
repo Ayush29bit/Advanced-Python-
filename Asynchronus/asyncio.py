afb9667f5d19385edbdb18aff3f9dd4ee17f35d2
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-05-10 16:58:41 +0530

    keywords and event loop

--- Asynchronus/asyncio.py
+++ Asynchronus/asyncio.py
@@ -47,4 +47,36 @@ Multiprocessing : way of acheiving parallelism by spreading tasks over a compute
 Concurrency : Multiple tasks having the ability to run in overlapping manner
 Threading : Concurrent execution model in which multiple threads take turns executing tasks
 """
+"""
+Concurrency is a concept and asyncio is a technique to implement it 
+Parallelism is a concept and multoprocessing is a technique to implement it 
+"""
 
+"""
+The asyncio package is billed by the Python documentation as a library to write concurrent code.
+However, async I/O isn’t threading or multiprocessing. It’s not built on top of either of these.
+Async I/O is a single-threaded, single-process technique that uses cooperative multitasking. 
+Async I/O gives a feeling of concurrency despite using a single thread in a single process.
+
+"""
+
+"""
+What does it mean to be Asynchronous?
+Asynchronous routines can pause their execution while waiting for a result and allow other routines to run in the meantime.
+Asynchronous code facilitates the concurrent execution of tasks by coordinating asynchronous routines.
+"""
+
+"""
+Async and Await and Coroutines
+
+The async def syntax construct introduces either a coroutine function or an asynchronous generator.
+The await keyword suspends the execution of the surrounding coroutine and passes control back to the event loop.
+"""
+
+
+""" 
+async def g():
+    result = await f()  # Pause and come back to g() when f() returns
+    return result
+    
+"""
