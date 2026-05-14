25bd39e07b6be8ca2751919b8876136c3d4d8afd
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-05-15 02:45:29 +0530

    basemodels

---
+++ Pydantic/model_01.py
@@ -0,0 +1,38 @@
+"""
+Pydantic is a data validation and settings management library for Python. 
+It allows you to define data models using Python classes, and it provides powerful features for validating and parsing data.
+Pydantic is often used in web development, data processing, and any situation where you need to ensure that the data you're working with adheres to a specific structure.
+"""
+from pydantic import BaseModel #type:ignore
+
+class Product(BaseModel):
+    id:int
+    name:str
+    price:float
+    in_stock:bool=True
+
+"""
+Pydantic uses type annotations to define the fields of the model.
+In this example, we have a Product model with four fields: id, name, price, and in_stock. The in_stock field has a default value of True.
+"""
+from pydantic import BaseModel, Field #type:ignore
+from typing import typing, List, Dict, Optional
+
+class Cart(BaseModel):
+    user_id:int
+    items:List[str]
+    quantities:Dict[str,int]
+
+class BlogPost(BaseModel):
+    title:str
+    content:str
+    image_url:Optional[str]=None
+
+"""
+Pydnatic also supports more complex data types, such as lists and dictionaries.
+In the Cart model, we have a list of items and a dictionary that maps item names to their quantities.
+The BlogPost model includes an optional field for an image URL, which can be None if not provided."""
+
+     
+
+
