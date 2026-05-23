ae7ec5876bdeb403f513dd88e3bbe0365193522f
Ayush Tiwari <ayushzztiwari69@gmail.com>
2026-05-23 18:50:27 +0530

    generators

--- oop/generators.py
+++ oop/generators.py
@@ -17,3 +17,38 @@ next(gen_values_object) # This will raise a StopIteration exception, as there ar
 
 for values in gen_values_object():
     print(values) # This will print the values yielded by the generator, one at a time.
+
+def get_primes_list(start,end):
+    primes = []
+    for num in range(start, end+1):
+        if num < 2 :
+            continue
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    return primes
+
+"""
+Basically, A generator is a special type of iterator that produces values one at a time instead of storing everything in memory at once.
+"""
+
+"""
+Example of memeory efficiency of generators:
+"""
+# List Comprehension
+squares = [x*x for x in range(1000000)]
+
+"""
+This creates the ENTIRE list immediately in memory.
+"""
+
+# Generator Expression
+squares_gen = (x*x for x in range(1000000))
+
+"""
+This creates a generator that produces squares one at a time, which is much more memory efficient.
+"""
